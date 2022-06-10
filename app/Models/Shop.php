13c84b96e946636ba3d0b4b454638d79be5081ea
raymondlay023 <raymondlay023@gmail.com>
2022-06-10 09:55:41 +0700

    first commit

---
+++ app/Models/Shop.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Shop extends Model
+{
+    use HasFactory, SoftDeletes;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'users_id',
+        'name',
+        'phone_number',
+        'category',
+    ];
+
+
+}
